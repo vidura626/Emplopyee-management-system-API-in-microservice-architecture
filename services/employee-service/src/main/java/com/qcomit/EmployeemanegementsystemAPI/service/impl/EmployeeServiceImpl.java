9d7a54fbd1797b85117d3b1dc05a664fe854b15e
vidura626 <wijesinghemap@gmail.com>
2023-11-14 15:01:17 +0530

    imports optimized

--- services/employee-service/src/main/java/com/qcomit/EmployeemanegementsystemAPI/service/impl/EmployeeServiceImpl.java
+++ services/employee-service/src/main/java/com/qcomit/EmployeemanegementsystemAPI/service/impl/EmployeeServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 
