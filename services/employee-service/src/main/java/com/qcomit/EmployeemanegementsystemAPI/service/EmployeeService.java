fee742690e26717a092b7595be910b0efac66ded
vidura626 <wijesinghemap@gmail.com>
2023-11-14 13:48:50 +0530

    imports optimized

--- services/employee-service/src/main/java/com/qcomit/EmployeemanegementsystemAPI/service/EmployeeService.java
+++ services/employee-service/src/main/java/com/qcomit/EmployeemanegementsystemAPI/service/EmployeeService.java
@@ -1,7 +1,6 @@
 package com.qcomit.EmployeemanegementsystemAPI.service;
 
 import com.qcomit.EmployeemanegementsystemAPI.dto.EmployeeDto;
-import io.jsonwebtoken.Claims;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
